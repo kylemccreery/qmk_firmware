1fde2580aa65e2159405ccc67db9b0c4c437f3b8
Kyle McCreery <mccreery.kyle@gmail.com>
2022-06-15 01:47:16 -0400

    Fixing audio definition issues that broke while refactoring for eeprom

--- keyboards/mechwild/waka60/f401/mcuconf.h
+++ keyboards/mechwild/waka60/f401/mcuconf.h
@@ -4,8 +4,6 @@
 #pragma once
 
 #include_next <mcuconf.h>
-#undef STM32_I2C_USE_I2C1
-#define STM32_I2C_USE_I2C1                  TRUE
 #undef STM32_PWM_USE_TIM1
 #define STM32_PWM_USE_TIM1                  TRUE
 #undef STM32_GPT_USE_TIM4
